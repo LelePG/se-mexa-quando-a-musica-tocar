b54a01b8392a1d45f3fdd735bde9444205827905
Letícia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2021-07-14 23:36:46 -0300

    primeiro commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+function menu(){
+        console.log("Selecione o intervalo:")
+        console.log("1) 15 minutos")
+        console.log("2) 20 minutos")
+        console.log("3) 30 minutos")
+        console.log("4) 40 minutos")
+        console.log("5) 50 minutos")
+        console.log("6) 60 minutos")
+        console.log("7) 1 hora e 15 minutos")
+        console.log("8) 1 hora e 30 minutos")
+        console.log("9) 2 horas")
+        console.log("10) 2 horas e 30 minutos")
+}
