cb6014c09268fe71ced7c5972b1872f48ef268bb
Letícia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2021-07-15 21:33:00 -0300

    Adicionado menu e agendamento da tarefa

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+const prompt = require('prompt-sync')();
+const cron = require('cron');
 
-function menu(){
+function menu() {
+        while(1){
         console.log("Selecione o intervalo:")
         console.log("1) 15 minutos")
         console.log("2) 20 minutos")
@@ -11,4 +14,26 @@ function menu(){
         console.log("8) 1 hora e 30 minutos")
         console.log("9) 2 horas")
         console.log("10) 2 horas e 30 minutos")
+
+        let escolha = Number(prompt(''))
+
+        switch (escolha) {
+                case 1: return 1
+                case 2: return 20
+                case 3: return 30
+                case 4: return 40
+                case 5: return 50
+                case 6: return 59
+                default: console.log("Opção inválida")
+        }
+        }
 }
+
+
+let minuto = menu()
+
+let cronJob = cron.job(`0 */${minuto} * * * *`, function(){
+    console.info("se mexe ai");
+}); 
+
+cronJob.start();
